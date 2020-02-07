252bbb45fbba88a192cdcf528e800128a4658ef4
Casandra <casandragrullon@pursuit.org>
2020-02-07 09:38:31 -0500

    added constraints for search bar

--- StudioGhibliApp/Controllers/MiyazakiVC.swift
+++ StudioGhibliApp/Controllers/MiyazakiVC.swift
@@ -10,21 +10,18 @@ import UIKit
 
 class MiyazakiVC: UIViewController {
 
+    private var miyazakiView = MiyazakiView()
+    
+    override func loadView() {
+        view = miyazakiView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- StudioGhibliApp/Views/SearchView.swift
+++ StudioGhibliApp/Views/SearchView.swift
@@ -10,12 +10,43 @@ import UIKit
 
 class SearchView: UIView {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    public lazy var searchBar: UISearchBar = {
+       let search = UISearchBar()
+        search.placeholder = "search for movie"
+        search.tintColor = .white
+        search.barTintColor = #colorLiteral(red: 0.4745098054, green: 0.8392156959, blue: 0.9764705896, alpha: 1)
+        return search
+    }()
+    public lazy var aboutLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.textColor = .white
+        return label
+    }()
+    
+
+    override init(frame: CGRect) {
+        super.init(frame: UIScreen.main.bounds)
+        commonInit()
     }
-    */
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        commonInit()
+    }
+    private func commonInit() {
+        searchBarConstraints()
+    }
+
+    private func searchBarConstraints() {
+        addSubview(searchBar)
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            searchBar.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            searchBar.leadingAnchor.constraint(equalTo: leadingAnchor),
+            searchBar.trailingAnchor.constraint(equalTo: trailingAnchor)
+        ])
+    }
+    
 
 }
