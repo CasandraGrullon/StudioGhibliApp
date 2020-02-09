568ca7128664efde0badc2f594ec45fc1d4aa3f0
Casandra <casandragrullon@pursuit.org>
2020-02-09 14:57:37 -0500

    added a function to create sections based on director

--- StudioGhibliApp/Controllers/SearchVC.swift
+++ StudioGhibliApp/Controllers/SearchVC.swift
@@ -12,17 +12,74 @@ class SearchVC: UIViewController {
 
     private var searchView = SearchView()
     
+    var ghiblis = [Ghibli]() {
+        didSet {
+            self.searchView.moviesCollection.reloadData()
+        }
+    }
+    var searchQuery = String() {
+        didSet {
+            ghiblis = ghiblis.filter {$0.title.lowercased() == searchQuery.lowercased()}
+        }
+    }
+    
+    var numberOfSections = [[Ghibli]]()
+    
     override func loadView() {
         view = searchView
+        makeSections()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
-
-
+    
+    private func getMovies() {
+        ghiblis = [GhibliAPIClient.getMovies()]
+    }
+    
+    private func makeSections() {
+        var miyazakiSection: [Ghibli]
+        var takahataSection: [Ghibli]
+        var otherSection: [Ghibli]
+        for movie in ghiblis {
+            if movie.director == "Hayao Miyazaki" {
+                miyazakiSection.append(movie)
+                numberOfSections.append(miyazakiSection)
+            } else if movie.director == "Isao Takahata" {
+                takahataSection.append(movie)
+                numberOfSections.append(takahataSection)
+            } else {
+                otherSection.append(movie)
+                numberOfSections.append(otherSection)
+            }
+        }
+    }
+    
 }
 extension SearchVC: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchQuery = textField.text ?? "Spirited Away"
+        textField.resignFirstResponder()
+        return true
+    }
+}
+extension SearchVC: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return numberOfSections[section].count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        
+        return numberOfSections.count
+    }
+    
+    
+}
+extension SearchVC: UICollectionViewDelegateFlowLayout {
     
 }
--- StudioGhibliApp/Network Services/GhibliAPIClient.swift
+++ StudioGhibliApp/Network Services/GhibliAPIClient.swift
@@ -9,3 +9,22 @@
 import Foundation
 import NetworkHelper
 
+struct GhibliAPIClient {
+    static func getMovies() -> Ghibli {
+        var ghibli: Ghibli!
+        guard let fileURL = Bundle.main.url(forResource: "GhibliJson", withExtension: "json") else {
+            fatalError("could not get url")
+        }
+        
+        do {
+           let data = try Data(contentsOf: fileURL)
+            let results = try JSONDecoder().decode(Ghibli.self, from: data)
+            ghibli = results
+        } catch {
+            print("decoding error \(error)")
+            
+        }
+        return ghibli
+    }
+}
+
